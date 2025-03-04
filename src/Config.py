90698ac31aa9492c9b200c56cd76cbe3dd0c23bc
qphu1905 <buiqphu1905@gmail.com>
2025-03-04 01:51:11 +0100

    feat(Config.py): Added Config.py for default values.

---
+++ src/Config.py
@@ -0,0 +1,6 @@
+class Config:
+    BROKER_ADDRESS = 'w1b0d181.ala.eu-central-1.emqxsl.com'
+    BROKER_PORT = 8883
+    CERTIFICATE_FILE = '../emqxsl-ca.crt'
+    USERNAME = 'quocphu'
+    PASSWORD = '190505'
