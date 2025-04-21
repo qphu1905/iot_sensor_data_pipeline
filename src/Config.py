f7f3230fd3b43f91baa0fe4a8b5ee3cb771da8f2
qphu1905 <quobui@uni-mainz.de>
2025-04-22 01:38:11 +0200

    REMOVE: remove Config.py (rip)

--- src/Config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    BROKER_ADDRESS:str = 'w1b0d181.ala.eu-central-1.emqxsl.com'
-    BROKER_PORT:int = 8883
-    CERTIFICATE_FILE:str = '../emqxsl-ca.crt'
-    USERNAME:str = 'quocphu'
-    PASSWORD:str = '190505'
-    TOPICS:list[str] = ['RAW_DATA']
-    BOOTSTRAP_SERVERS: list[str]= ['localhost:9092']
