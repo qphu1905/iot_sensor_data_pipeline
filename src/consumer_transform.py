ebd7dc1203a67abd4c44d839df6fa79ba5c913e8
qphu1905 <quobui@uni-mainz.de>
2025-05-01 23:20:44 +0200

    ADD: Add function to calculate warning

--- src/consumer_transform.py
+++ src/consumer_transform.py
@@ -58,6 +58,27 @@ def calculate_apparent_temperature(temperature: float, humidity: float) -> float
             return apparent_temperature
 
 
+def warning(apparent_temperature: float) -> int:
+    """Calculate warning level for given apparent temperature.
+    :parameter: apparent_temperature: float
+    :return: warning_level: int
+    """
+
+    if apparent_temperature <= 27:
+        warning_level = 0
+    elif 27.0 < apparent_temperature <= 32.0:
+        warning_level = 1
+    elif 32.0 < apparent_temperature <= 39.0:
+        warning_level = 2
+    elif 39.0 < apparent_temperature <= 51.0:
+        warning_level = 3
+    elif 51.0 < apparent_temperature <= 58.0:
+        warning_level = 4
+    else:
+        warning_level = 5
+    return warning_level
+
+
 def transform(message: dict) -> dict:
     """Transform and enrich raw data from sensor
      :parameter: message: dict: raw data from sensor
@@ -75,6 +96,7 @@ def transform(message: dict) -> dict:
 
     #transform message
     message['feels_like_temperature'] = feels_like_temperature
+    message['warning_level'] = warning(feels_like_temperature)
     message['time_id'] = time_id
     message['date_id'] = date_id
     return message
